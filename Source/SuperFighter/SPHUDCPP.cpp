2678c3ed68b6f8bc7a67e9e04015671cbbd78679
BenqGG <35500873+BenqGG@users.noreply.github.com>
2018-05-29 20:26:37 +0200

    3344

--- Source/SuperFighter/SPHUDCPP.cpp
+++ Source/SuperFighter/SPHUDCPP.cpp
@@ -13,25 +13,26 @@ ASPHUDCPP::ASPHUDCPP()
 void ASPHUDCPP::BeginPlay()
 {
 	Super::BeginPlay();
-
+	/*NOT WORKING DUNNO WHY
 	if (HasAuthority()) {
-
+		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Yellow, "NO HUD");
 	}
 	else {
-		if (wMainHUD) // Check if the Asset is assigned in the blueprint.
+		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Yellow, "YES HUD");
+		if (IsValid(wMainHUD)) // Check if the Asset is assigned in the blueprint.
 		{
 			// Create the widget and store it.
 			MainHUD = CreateWidget<UUserWidget>(UGameplayStatics::GetPlayerController(GetWorld(), 0), wMainHUD);
 
 			// now you can use the widget directly since you have a referance for it.
 			// Extra check to  make sure the pointer holds the widget.
-			if (MainHUD)
+			if (IsValid(MainHUD))
 			{
 				//let add it to the view port
 				MainHUD->AddToViewport();
 			}
 		}
-	}
+	}*/
 }
 
 void ASPHUDCPP::Tick(float DeltaSeconds)
